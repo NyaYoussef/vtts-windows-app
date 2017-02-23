46364b748809a33afb68378c94cbcf9759f8518a
ESSARRAJ <essarraj.fouad@gmail.com>
2017-02-23 15:24:01 +0000

    Integration of genetic winform application

--- VttsTests/BaseRepositoryTests.cs
+++ VttsTests/BaseRepositoryTests.cs
@@ -1,12 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using App;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Entities.TraineeManagement;
-using App.WinForm.Application.BAL;
+using App;
 
 namespace App.Tests
 {
