33d03b6a273b0ba995e0a429d87a58928a15e7c9
NyaYoussef <ussef9663@outlook.fr>
2017-05-06 11:07:37 +0200

    Write into method seed

--- VttsTests/BaseRepositoryTests.cs
+++ VttsTests/BaseRepositoryTests.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Entities.TraineeManagement;
 using App;
+using vtts.BAL;
+using App.Gwin.Entities.Secrurity.Authentication;
+using App.Gwin;
+using vtts;
 
 namespace App.Tests
 {
@@ -12,12 +16,17 @@ namespace App.Tests
         [TestMethod()]
         public void InsertTest()
         {
-            int Expected = 1;
-            Group  groupe = new Group();
-            groupe.Name = "groupe 1";
-            IBaseBLO service = new IBaseBLO<Group>();
-            int Actuel = service.Save(groupe);
-            Assert.AreEqual(Expected, Actuel);
+            //    int Expected = 1;
+            //    Group  groupe = new Group();
+            //    groupe.Name = "groupe 1";
+            //    vtts.BAL.BaseBLO service = new BaseBLO<Group>();
+            //    int Actuel = service.Save(groupe);
+            //    Assert.AreEqual(Expected, Actuel);
+            User user = new User();
+            user.Language = GwinApp.Languages.en;
+            GwinApp.Start(typeof(ModelContext), typeof(BaseBLO<>), new FormMenu(), user);
+
+
 
         }
     }
--- vtts/Program.cs
+++ vtts/Program.cs
@@ -1,9 +1,4 @@
-﻿
-using App.WinForm.Fields.TestFields;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 
 namespace vtts
