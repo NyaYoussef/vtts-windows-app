da399016b5bd10bf98daf7d5b2eb6ce269e4d42c
ESSARRAJ <essarraj.fouad@gmail.com>
2017-05-10 13:15:36 +0100

    Add Spire Library

---
+++ vtts/Presentation/PrintOrderMission/FormPrintOrderMission.cs
@@ -0,0 +1,57 @@
+﻿using App.Gwin.Entities.Secrurity.Authentication;
+using Spire.Pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vtts.Presentation.PrintOrderMission
+{
+    [App.Gwin.Attributes.Menu( EntityType = typeof(User))]
+    public partial class FormPrintOrderMission : Form
+    {
+        public FormPrintOrderMission()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PdfDocument doc = new PdfDocument();
+            doc.LoadFromFile("sample.pdf");
+
+            //Use the default printer to print all the pages 
+            //doc.PrintDocument.Print(); 
+
+            //Set the printer and select the pages you want to print 
+
+            PrintDialog dialogPrint = new PrintDialog();
+            dialogPrint.AllowPrintToFile = true;
+            dialogPrint.AllowSomePages = true;
+            dialogPrint.PrinterSettings.MinimumPage = 1;
+            dialogPrint.PrinterSettings.MaximumPage = doc.Pages.Count;
+            dialogPrint.PrinterSettings.FromPage = 1;
+            dialogPrint.PrinterSettings.ToPage = doc.Pages.Count;
+
+            if (dialogPrint.ShowDialog() == DialogResult.OK)
+            {
+                //Set the pagenumber which you choose as the start page to print 
+                doc.PrintFromPage = dialogPrint.PrinterSettings.FromPage;
+                //Set the pagenumber which you choose as the final page to print 
+                doc.PrintToPage = dialogPrint.PrinterSettings.ToPage;
+                //Set the name of the printer which is to print the PDF 
+                doc.PrinterName = dialogPrint.PrinterSettings.PrinterName;
+
+                PrintDocument printDoc = doc.PrintDocument;
+                dialogPrint.Document = printDoc;
+                printDoc.Print();
+            }
+        }
+    }
+}
