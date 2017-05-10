5fbfd8f33352df7bef2bedba4d197f2db7046c8f
ESSARRAJ <essarraj.fouad@gmail.com>
2017-05-10 13:25:30 +0100

    Update GwinLibrary

--- vtts/Presentation/PrintOrderMission/FormPrintOrderMission.Designer.cs
+++ vtts/Presentation/PrintOrderMission/FormPrintOrderMission.Designer.cs
@@ -39,7 +39,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(54, 93);
+            this.button1.Location = new System.Drawing.Point(364, 42);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
