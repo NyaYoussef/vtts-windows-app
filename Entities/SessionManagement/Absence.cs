54d1063d2d29807cf415866f5bf9a216ab632acf
NyaYoussef <ussef9663@outlook.fr>
2017-05-11 13:50:50 +0200

    add Gwin v12

---
+++ Entities/SessionManagement/Absence.cs
@@ -0,0 +1,16 @@
+﻿using App.Gwin.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.SessionManagement
+{
+    public class Absence:BaseEntity
+    {
+        public DateTime AbsenceDate { get; set; }
+        public string CauseOfAbsence { get; set; }
+
+    }
+}
