c58f002e30d7109a13ff2cfa3c12ab6773e44a43
AitalMariam <Aital_1998@outlook.com>
2017-05-17 23:17:59 +0100

    commit17052017

--- vtts/Presentation/PrintOrderMission/FormPrintOrderMission.cs
+++ vtts/Presentation/PrintOrderMission/FormPrintOrderMission.cs
@@ -42,9 +42,39 @@ namespace vtts.Presentation.PrintOrderMission
         {
             vtts.BAL.MissionManagement.PrintOrderMission printOrderMission = new vtts.BAL.MissionManagement.PrintOrderMission();
             //
+            printOrderMission.HeaderImgDirectory = @"C:\USers\DELL\Desktop\vtts-windows-app\Images\Header.png";
             printOrderMission.Ordre = "OFP/DRNOII/ISMONTIC/OM/N° 13/17";
             printOrderMission.Date = DateTime.Now;
+            printOrderMission.Region = "Le Directeur Regional de la DRNOII";
+            printOrderMission.City = "Tanger";
+            printOrderMission.Mensieur = "Monsieur   :   Bouybanin Anass";
+            printOrderMission.Matricule = " 13716";
+            printOrderMission.Category = "Cadre Principale";
+            printOrderMission.Affectation = "I.S.M.O.N.T.I.C Tanger";
+            printOrderMission.Place = "CDC TIC Casa Blanca";
+            printOrderMission.Theme = "Pour assister aux atelier NETACAD CISCO";
+            printOrderMission.DepartureDate = "24/04/2017";
+            printOrderMission.ReturnDate = "29/04/2017";
+            printOrderMission.DepartureHour = "13h00";
+            printOrderMission.ReturnHour = "13h00";
+            //PublicTransport , MissionCar, PersonalCar
+            printOrderMission.TransportType = "PublicTransport";
+            // In Other Cases
+            //if(printOrderMission.TransportType == "MissionCar")
+            //{
+            //    printOrderMission.MissionCarmark = "Mark";
+            //    printOrderMission.MissionCarPlatNumber = "Numero de plaque";
+            //}
+            //if(printOrderMission.TransportType == "PersonalCar")
+            //{
+            //    printOrderMission.PersonalCarmark = "Mark";
+            //    printOrderMission.PersonalCarPlatNumber = "Numero de plaque";
+            //    printOrderMission.PersonalCarFiscalPower = "Puissance Fiscale";
+            //}
             //
+
+            printOrderMission.FirstPersonne = "Abdelhamid ELMECHRAFI";
+            printOrderMission.SecondePersonne = "Abdelmoula SADIK";
             this.PathPDF = printOrderMission.CreatePDF();
 
 
